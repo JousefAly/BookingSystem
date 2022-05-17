6830d66e7f8d28ff0d2b764e970ff7550d69a43d
JousefAly <youssefali00286@gmail.com>
2022-05-17 05:11:20 +0200

    Created Core Projects as layers

---
+++ src/BookingSystem_BAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BookingSystem_BAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/BookingSystem_DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BookingSystem_DAL
+{
+    public class Class1
+    {
+    }
+}
