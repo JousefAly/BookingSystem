379825cae2cef87a9efaa242a9717b78ace362f4
JousefAly <youssefali00286@gmail.com>
2022-05-17 06:37:34 +0200

    Added EntityFrameWork and Context

---
+++ src/BookingSystem_DAL/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using BookingSystem_DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem_DAL.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder OptBuilder)
+        {
+            OptBuilder
+                .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=BookingDatabase;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
