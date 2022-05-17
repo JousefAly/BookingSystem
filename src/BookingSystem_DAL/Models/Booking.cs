8fb0feebc954e5c0215b8d51f287d59956053613
JousefAly <youssefali00286@gmail.com>
2022-05-17 05:56:56 +0200

    Added DAL Models

---
+++ src/BookingSystem_DAL/Models/Booking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem_DAL.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public int GuestId { get; set; }
+        public Guest Guest { get; set; }
+        public List<Room> Rooms { get; set; }
+    }
+}
---
+++ src/BookingSystem_DAL/Models/Room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem_DAL.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Type { get; set; }
+
+        public int BranchId { get; set; }
+        public Branch Branch { get; set; }
+    }
+}
