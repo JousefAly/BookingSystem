88d49053ae74068846471c87c6db4723c7bddabe
JousefAly <youssefali00286@gmail.com>
2022-05-19 16:14:18 +0200

    Added Guest Repository w8 to implement methods

---
+++ src/BookingSystem_DAL/Repositories/GuestRepository.cs
@@ -0,0 +1,38 @@
+﻿using BookingSystem_DAL.Contracts;
+using BookingSystem_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem_DAL.Repositories
+{
+    public class GuestRepository : IRepository<Guest>
+    {
+        public Guest Create(Guest _object)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Guest _object)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Guest> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guest GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guest Update(Guest _object)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
